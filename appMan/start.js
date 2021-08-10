9e200c9179f35ffe624ba14368d806e4d0974014
Perodactyl <81657800+Perodactyl@users.noreply.github.com>
2021-08-10 17:34:49 -0400

    Create start.js
    Start.js simply opens a window of App Manager, so that a shortcut could open it.

---
+++ appMan/start.js
@@ -0,0 +1,9 @@
+//This file belongs in /a/appMan! Make sure it stays there!
+//Run this to open a window.
+// I would use it to make a desktop shortut, linking to:
+//  js /a/appMan/start.js
+
+
+
+
+startAppMan()

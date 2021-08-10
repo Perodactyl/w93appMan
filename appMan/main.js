4b989e550875c9d9a71ac14fdfb8ccf090c806d8
Perodactyl <81657800+Perodactyl@users.noreply.github.com>
2021-08-10 10:31:11 -0400

    Update main.js
    Added lots of stuff.

--- appMan/main.js
+++ appMan/main.js
@@ -1,6 +1,30 @@
 //This one goes in /a/appMan! Make sure it stays there!
 var container = document.getElementById("appMan-container")
-container.innerHTML = "<h3>App Manager</h3>Hello. Welcome to App Manager! This is really WIP.<button onclick=\"uninstallappMan()\">Uninstall</button>"
+var list = document.getElementById("appMan-appList")
+$log("AppMan process running.")
+var foundApps = [
+  {
+  	name:"test",
+    type:"js",
+    desc:`A test thingy`,
+    fs:{
+    	"foobar.js":"$notif(It's running!)"
+    }
+  }
+]
+list.innerHTML = ""
+for(let idx in foundApps){
+  	let data = foundApps[idx]
+	let el = document.createElement("div")
+    el.className = "appMan-app"
+    //Style handled by /a/boot/appMan.css
+    el.innerHTML = `<span class="appMan-app-title">${data.name}</span>
+<span class="appMan-app-description">${data.desc.length > 50 ? data.desc.slice(0,50)+"..." : data.desc}</span>
+<span class="appMan-app-type">${data.type}</span>`
+    list.append(el)
+}
+
+
 window.uninstallappMan = function(){
   	console.log("Deleting...")
 	  $file.delete("/a/boot/appMan.js")

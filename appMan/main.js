dd7aa0315368ff6f7e34edd7eb2783196b20640a
Perodactyl <81657800+Perodactyl@users.noreply.github.com>
2021-08-10 02:25:31 -0400

    Update main.js
    Added uninstall (very temporary)

--- appMan/main.js
+++ appMan/main.js
@@ -1,3 +1,12 @@
 //This one goes in /a/appMan! Make sure it stays there!
 var container = document.getElementById("appMan-container")
-container.innerHTML = "<h3>App Manager</h3>Hello. Welcome to App Manager! This is really WIP."
+container.innerHTML = "<h3>App Manager</h3>Hello. Welcome to App Manager! This is really WIP.<button onclick=\"uninstallappMan()\">Uninstall</button>"
+window.uninstallappMan = function(){
+  	console.log("Deleting...")
+	  $file.delete("/a/boot/appMan.js")
+    $file.delete("/a/appMan/main.js")
+    $file.delete("/a/appMan/main.html")
+    $file.delete("/a/appMan/")
+    $notif("Uninstalled App Manager!")
+    $log("Attempt to uninstall app manager.")
+}
